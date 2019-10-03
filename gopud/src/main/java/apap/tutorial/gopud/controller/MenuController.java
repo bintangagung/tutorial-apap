ee859ab6f48c48168f74f071eb70efc8f512d147
bintang agung nusantara <bintangagung1505@gmail.com>
2019-10-03 14:58:07 +0700

    replace:README.md

--- gopud/src/main/java/apap/tutorial/gopud/controller/MenuController.java
+++ gopud/src/main/java/apap/tutorial/gopud/controller/MenuController.java
@@ -67,21 +67,6 @@ public class MenuController {
         return "change-menu-restoran";
     }
 
-    //delete menu
-//    @RequestMapping(path = "/restoran/menu/delete/{id}", method = RequestMethod.GET)
-//    public String deleteMenu(@PathVariable Long id, Model model) {
-//        // Mengambil objek MenuModel yang dituju
-//        MenuModel menu = menuService.getMenuById(id).get();
-////        if (menu.get RestoranService.getRestoranByIdRestoran(idRestoran).get().isEmpty()) {
-////            model.addAttribute("menu", menu);
-////            return "delete-menu-gagal";
-////        }
-////        else {
-//            menuService.deleteMenu(menu);
-//            model.addAttribute("menu", menu);
-//            return "delete-menu";
-////        }
-//    }
     @RequestMapping(value="/restoran/menu/delete", method = RequestMethod.POST)
     private String delete(@ModelAttribute RestoranModel restoran, Model model) {
         for (MenuModel menu : restoran.getListMenu()) {
