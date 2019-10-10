b3dfa51719b380f18fae7a06b357aae8546125d9
ctampan <narvaneon@yahoo.com>
2019-10-10 20:32:24 +0700

    menuservice test

---
+++ gopud/src/test/java/apap/tutorial/gopud/service/MenuServiceImplTest.java
@@ -0,0 +1,124 @@
+package apap.tutorial.gopud.service;
+
+import apap.tutorial.gopud.model.MenuModel;
+import apap.tutorial.gopud.model.RestoranModel;
+import apap.tutorial.gopud.repository.MenuDb;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MenuServiceImplTest {
+    @InjectMocks
+    MenuService menuService = new MenuServiceImpl();
+
+    @Mock
+    MenuDb menuDb;
+
+    private RestoranModel createDummyRestoran(){
+        RestoranModel restoran = new RestoranModel();
+        restoran.setNama("mekdi");
+        restoran.setAlamat("Kemiri Muka");
+        restoran.setNomorTelepon(14045);
+
+        return restoran;
+    }
+
+    private MenuModel createDummyMenu(){
+        MenuModel menu = new MenuModel();
+        menu.setNama("ayam");
+        menu.setHarga(BigInteger.valueOf(15000));
+        menu.setDurasiMasak(10);
+        menu.setDeskripsi("enak");
+
+        return menu;
+    }
+
+    @Test
+    public void whenAddValidMenuItShouldCallMenuRepositorySave(){
+        MenuModel newMenu = createDummyMenu();
+
+        menuService.addMenu(newMenu);
+
+        verify(menuDb, times(1)).save(newMenu);
+    }
+
+    @Test
+    public void whenDeleteValidMenuItShouldCallMenuRepositoryDelete(){
+        MenuModel newMenu = createDummyMenu();
+
+        menuService.addMenu(newMenu);
+
+        menuService.deleteMenu(newMenu);
+
+        verify(menuDb, times(1)).delete(newMenu);
+    }
+
+    @Test
+    public void whenGetMenuByIdRestoranCalledByExistingDataItShouldReturnCorrectData(){
+        RestoranModel restoran = createDummyRestoran();
+        restoran.setIdRestoran(1L);
+        MenuModel menu = createDummyMenu();
+        menu.setId(1L);
+
+        when(menuService.getMenuById(1L)).thenReturn(Optional.of(menu));
+
+        Optional<MenuModel> dataFromServiceCall = menuService.getMenuById(1L);
+
+        verify(menuDb, times(1)).findById(1L);
+        assertTrue(dataFromServiceCall.isPresent());
+
+        MenuModel dataFromOptional = dataFromServiceCall.get();
+
+        assertEquals("ayam", dataFromOptional.getNama());
+        assertEquals(BigInteger.valueOf(15000), dataFromOptional.getHarga());
+        assertEquals(Integer.valueOf(10), dataFromOptional.getDurasiMasak());
+        assertEquals("enak", dataFromOptional.getDeskripsi());
+    }
+
+    @Test
+    public void whenGetMenuListByIdRestoranCalledItShouldReturnAllMenu(){
+        RestoranModel restoranModel = createDummyRestoran();
+        restoranModel.setIdRestoran(1L);
+        List<MenuModel> allMenuInRestoran = new ArrayList<>();
+        for(int loopTimes = 3; loopTimes > 0; loopTimes--){
+            allMenuInRestoran.add(new MenuModel());
+        }
+        when(menuService.findAllMenuByIdRestoran(1L)).thenReturn(allMenuInRestoran);
+
+        List<MenuModel> dataFromServiceCall = menuService.findAllMenuByIdRestoran(1L);
+        assertEquals(3, dataFromServiceCall.size());
+
+        verify(menuDb, times(1)).findByRestoranIdRestoran(1L);
+    }
+
+    @Test
+    public void whenChangeMenuCalledItShouldChangeMenuData(){
+        MenuModel updatedData = createDummyMenu();
+        updatedData.setId((long)1);
+        updatedData.setDeskripsi("mantab");
+
+        when(menuDb.findById(1L)).thenReturn(Optional.of(updatedData));
+
+        when(menuService.changeMenu(updatedData)).thenReturn(updatedData);
+
+        MenuModel dataFromServiceCall = menuService.changeMenu(updatedData);
+
+        assertEquals("ayam", dataFromServiceCall.getNama());
+        assertEquals(BigInteger.valueOf(15000), dataFromServiceCall.getHarga());
+        assertEquals(Integer.valueOf(10), dataFromServiceCall.getDurasiMasak());
+        assertEquals("mantab", dataFromServiceCall.getDeskripsi());
+    }
+    
+}
