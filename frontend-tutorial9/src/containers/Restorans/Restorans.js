847f4bb467f25b9e24f33b4ec9493d77d753de10
bintang agung nusantara <bintangagung1505@gmail.com>
2019-11-27 11:46:50 +0700

    add:sampaiHalaman31

--- frontend-tutorial9/src/containers/Restorans/Restorans.js
+++ frontend-tutorial9/src/containers/Restorans/Restorans.js
@@ -1,6 +1,8 @@
 import React , { Component } from 'react' ;
+import classes from "./Restoran.module.css";
 
 class Restorans extends Component {
+
     constructor(props){
         super(props);
         this.state = {
@@ -8,38 +10,56 @@ class Restorans extends Component {
             [
                 {id: 1, nama: "Restoran A", alamat: "This is Address Restoran A", nomorTelepon: "021735313"},
                 {id: 1, nama: "Restoran B", alamat: "This is Address Restoran B", nomorTelepon: "021120491"},
-                {id: 1, nama: "Restoran C", alamat: "This is Address Restoran C", nomorTelepon: "021940256"},
+                {id: 1, nama: "Restoran C", alamat: "This is Address Restoran C", nomorTelepon: "021940256"}
             ],
-            isLoading: false
+            isLoading: true,
         }
     }
 
-    componentDidMount() {
-        console.log("componentDidMount()");
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("shouldComponentUpdate()");
-        return true; //ditambahin
-    }
-
-    loadingHandler = () => {
-        const currentIsLoading = this.state.isLoading;
-        this.setState({isLoading: !(currentIsLoading)} );
-        console.log(this.state.isLoading);
-    }
-    
     render (){
-        console.log("render()");
         return (
-        < div >
-            < div > Hello! Welcome to Gopud </ div >
-            < div > All Restorans </ div >
-            < div > Restorans 1, 2, 3, etc </ div >
-            <button onClick={this.loadingHandler}>change State </button>
-        </ div >
+        < React.Fragment >
+            <div className={classes.Title}> All Restorans </div>
+            <div className={classes.Restorans}>
+                {this.state.restorans.map(restoran =>
+                    <Restoran 
+                        key={restoran.id}
+                        nama={restoran.nama}
+                        alamat={restoran.alamat}
+                        nomorTelepon={restoran.nomorTelepon}
+                    />
+                )}
+            </div>
+        </ React.Fragment >
         );
     }
+
+    // componentDidMount() {
+    //     console.log("componentDidMount()");
+    // }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log("shouldComponentUpdate()");
+    //     return true; //ditambahin
+    // }
+
+    // loadingHandler = () => {
+    //     const currentIsLoading = this.state.isLoading;
+    //     this.setState({isLoading: !(currentIsLoading)} );
+    //     console.log(this.state.isLoading);
+    // }
+    
+    // render (){
+    //     console.log("render()");
+    //     return (
+    //     < div >
+    //         < div > Hello! Welcome to Gopud </ div >
+    //         < div > All Restorans </ div >
+    //         < div > Restorans 1, 2, 3, etc </ div >
+    //         <button onClick={this.loadingHandler}>change State </button>
+    //     </ div >
+    //     );
+    // }
 }
 
 export default Restorans ;
